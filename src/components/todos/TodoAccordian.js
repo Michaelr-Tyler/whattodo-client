4b971ee8b522328bb3910a674eec9a7892376027
Michael-Tyler <miketylerabc@gmail.com>
2021-01-07 14:05:00 -0600

    rid of usless button component, and don't have multiple server calls each page load

--- src/components/todos/TodoAccordian.js
+++ src/components/todos/TodoAccordian.js
@@ -1,27 +1,15 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { Fragment } from "react";
 import { Accordion, Card, Button, ListGroup, Row } from "react-bootstrap";
-import { CategoryContext } from "../category/CategoryDataProvider";
 import { TodoTagList } from "../todotags/TodoTagsList";
 import { Todo } from "./Todo";
-import { TodoContext } from "./TodoDataProvider";
 
-const TodoAccordian = () => {
-    const {todos, getTodos} = useContext(TodoContext)
-    const {categories, getCategories} = useContext(CategoryContext)
-
-    useEffect(()=>{
-        getTodos()
-        getCategories()
-        
-    },[])
-
-    
+const TodoAccordian = (props) => {
 
     return (
         <Accordion className="bg-dark mb-2 mt-4">
             <Card>
-                {categories.map((c)=>{
+                {props.categories.map((c)=>{
                     return(<Fragment key={c.id}>
                         <Card.Header className="bg-secondary">
                             <Accordion.Toggle as={Button} variant="secondary" eventKey={c.id}>
@@ -29,13 +17,13 @@ const TodoAccordian = () => {
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey={c.id}>
-                            <ListGroup variant="flush">{todos.map((td)=>{
+                            <ListGroup variant="flush">{props.todos.map((td)=>{
                                 if (td.category.id === c.id) {
                                     return (
                                         <ListGroup.Item className="bg-light">
                                             <Row>
                                                 <Todo key={td.id} task={td.task}/>
-                                                <TodoTagList onClick={() => console.log("clicked here too")} todoTags={td.tags}/>
+                                                <TodoTagList setTagId={props.setTagId} todoTags={td.tags}/>
                                             </Row>
                                         </ListGroup.Item>
                                     )
--- src/components/todotags/TodoTagsList.js
+++ src/components/todotags/TodoTagsList.js
@@ -3,17 +3,29 @@ import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 
-export const TodoTagList = ({ todoTags, settingTagId }) => {
+export const TodoTagList = (props) => {
+
+  props.todoTags.sort((a, b) => {
+    const nameA = a.label.toUpperCase();
+    const nameB = b.label.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
     <Col className="d-flex">
-        {todoTags.map((todoTag) => {
+        {props.todoTags.map((todoTag) => {
           return (
             <div className="d-flex align-items-center">
             <Badge as={Button} pill 
               onClick={(e)=>{ 
                 e.preventDefault() 
-                settingTagId(todoTag.id)
+                props.setTagId(todoTag.id)
               }} 
               variant="primary" 
               className="ml-1" 
