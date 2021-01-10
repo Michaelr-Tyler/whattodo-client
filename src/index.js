7a387e39405b092372b7645e8cf032c479ef3dd7
Michael-Tyler <miketylerabc@gmail.com>
2021-01-10 17:03:36 -0600

    commit to get heroku to update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { WhatToDo } from './components/WhatToDo';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
